e46cec18cf51462a092c2e662bb1da2c94caa870
dgc08 <83305972+dgc08@users.noreply.github.com>
2021-04-28 09:08:59 +0200

    Add files via upload

---
+++ cos-sys/__init__.py
@@ -0,0 +1,10 @@
+import os
+import sys
+from . import cnstall
+from . import cshell
+
+__version__ = 1.0.0
+
+shell = cshell.shell
+bash = shell
+cmd = shell
---
+++ setup.py
@@ -0,0 +1,29 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="cos-sys",
+    version="1.0.0",
+    description="OS-extension",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dgc08/cos",
+    author="DGC-08",
+    author_email="digc0820@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=[],
+    include_package_data=True,
+    install_requires=[],
+)
